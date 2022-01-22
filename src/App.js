cafb578dcc219b32cfeaa2358d0eef121b22fe2e
ayushkumar32539 <ayush2010128@akgec.ac.in>
2022-01-22 18:50:45 +0530

    basic

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import weather from './components/basics/weather'
+
+const App = () => {
+  return (
+    <>
+     weather
+    </>
+  )
+}
+
+export default App
---
+++ src/components/basics/weather.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../basics/style.css"
+const weather = () => {
+    return (
+        <>
+        <div className="header">
+            <div className="Search"></div>
+                <input type="Searh" placeholder='location....'/>
+        </div>
+            
+        </>
+    )
+}
+
+export default weather
